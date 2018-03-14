d8bfd794f62220c08aede8f11fc12078dca91775
Vagner Domingues <vagdom@hotmail.com>
2018-03-14 12:57:53 -0400

    Update linkedListType.h

--- CPP/linkedListType.h
+++ CPP/linkedListType.h
@@ -19,5 +19,36 @@ public:
   void initializeList();
   //Function to initialize the list to an empty state.
   //Postcondition: first = NULL; last = NULL;
-  //               count = 0.                                       
+  //               count = 0.
+                                        
+  bool isEmptyList();
+  //Function to determine whether the list is empty.
+  //Postcondition: Returns true if the list is empty;
+  //               otherwise, returns false.
+  
+  int length();
+  //Function to return the number of nodes in the
+  //list.
+  //Postcondition: The value of count is returned.
+                                        
+  void destroyList();
+  //Function to delete all the nodes from the list.
+  //Postcondition: first = NULL; last = NULL;
+  //               count = 0.   
+                                        
+  Type front();
+  //Function to return the first element of the list.
+  //Precondition: The list must exist and nust not
+  //be empty.
+  //Postcondition: If the list is empty, then the 
+  //               program terminates, otherwise, the first
+  //               element of the list is returned.  
+                                        
+  Type back();
+  //Function to return the last element of the list.
+  //Precondition: The list must exist and nust not
+  //be empty.
+  //Postcondition: If the list is empty, then the 
+  //               program terminates, otherwise, the last
+  //               element of the list is returned.                                        
 };
