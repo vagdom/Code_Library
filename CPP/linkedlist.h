e5fa62fd42a299ab8261697a27bc9449bb1ee7db
vagdom <vagdom@hotmail.com>
2018-03-13 21:06:21 -0400

    Update linkedlist.h

--- CPP/linkedlist.h
+++ CPP/linkedlist.h
@@ -1,1 +1,19 @@
 //Definition of the node
+
+template<class Type>
+struct nodeType
+{
+  Type info;
+  nodeType<Type> *link;
+};
+
+template<class Type>
+class linkedListType
+{
+  friend ostream& operator<<(ostream&, const linkedListType<Type>&);
+  //Overload the stream insertion operator.
+public:
+  const linkedListType<Type>& operator=(const linkedListType(Type>&);
+  //Overload the assignment operator.
+
+};
